9c97452acb104db481ed434ec45621086c2780a7
Nattalic <nordonez376@gmail.com>
2025-08-22 19:06:17 -0500

    finish 5 exercise

--- ejercicio-5.js
+++ ejercicio-5.js
@@ -3,4 +3,39 @@ class CuentaBancaria {
         this.titular = titular
         this.saldo = saldo
     }
+
+
+    mostrarSaldo() {
+        return `Mi saldo es: ${this.saldo}` //siempre hay que retornar algo ya que por defecto saldra undefind
+    }
+
+
+    depositarDinero(dinero) {
+        if(dinero > 0) {
+            this.saldo += dinero  //+= para guardar el nuevo saldo despues de sumarlo con dinero
+            console.log(`Usted ha depositado ${dinero}`)
+            console.log(this.mostrarSaldo())
+        }else {
+            console.log("dinero depositado incorrectamente")
+        }
+    }
+
+
+    retirarDinero(dineroARetirar) {
+        if (dineroARetirar < this.saldo) {
+             this.saldo -= dineroARetirar //toma el saldo anterior y le resta el dinero que quiere retirar
+            console.log(this.mostrarSaldo())
+            return dineroARetirar
+        } else {
+            console.log("saldos insuficientes")
+            return this.saldo
+        }
+ 
+    }
 }
+
+const miCuenta = new CuentaBancaria("Juan Pablo" , 1000) //instancia
+
+console.log(miCuenta.mostrarSaldo())
+miCuenta.depositarDinero(1000)
+miCuenta.retirarDinero(1000)
